c8056d1889a206f93b629bb126d3bd98da2a44c8
Orestis Zinelis <zinelisorestis@gmail.com>
2024-12-16 18:29:40 +0200

    Set gatsby-source-contentful in gatsby-config
    Set gatsby-source-contentful & netlifyAdapter. Add env variables

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,8 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby'
+import netlifyAdapter from 'gatsby-adapter-netlify'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -9,19 +13,38 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png'
+      }
     },
-    __key: "images"
-  }]
-};
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/'
+      },
+      __key: 'images'
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    }
+  ],
+  adapter: netlifyAdapter({
+    excludeDatastoreFromEngineFunction: false,
+    imageCDN: false
+  })
+}
 
-export default config;
+export default config
